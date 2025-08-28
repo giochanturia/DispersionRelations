901dd58a2cfb9a9be52767dc30e9211b69b2cd3a
Gio Chanturia <giochanturia@github.com>
2025-08-28 16:39:54 +0200

    Added Github workflows and more unit tests.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,12 +1,38 @@
+import numpy as np
 from dispersionrelations.constants import *
+
 
 def test_energy_unit_conversion():
     assert GeV / MeV == 1e3
     assert MeV / eV == 1e6
     assert GeV / keV == 1e6
     assert TeV / GeV == 1e6
+    assert 90 * degrees == np.pi / 2
+
+
+def test_scientific_notation():
+    assert scientific_notation(np.pi) == "3.14"
+    assert scientific_notation(np.pi, 10) == "3.1415926536"
+    assert scientific_notation(13.4) == r"1.34 \times 10^{1}"
+    assert scientific_notation(13.4, 10) == r"1.34 \times 10^{1}"
+    assert scientific_notation(0.134) == r"1.34 \times 10^{-1}"
+    assert scientific_notation(0.000134) == r"1.34 \times 10^{-4}"
+    assert scientific_notation(13495) == r"1.35 \times 10^{4}"
+    assert scientific_notation(13495, 20) == r"1.3495 \times 10^{4}"
 
 
 def test_rounding():
     a1, b1 = rounding_PDG(0.827, 0.367)
     assert a1 == 0.8 and b1 == 0.4
+    a2, b2 = rounding_PDG(0.827, 0.119)
+    assert a2 == 0.83 and b2 == 0.12
+
+
+def test_sR():
+    M1 = 500
+    G1 = 100
+    s1 = sR(M1, G1)
+    assert np.real(np.sqrt(s1)) == M1
+    assert np.imag(np.sqrt(s1)) == -G1 / 2
+    assert np.real(s1) == M1**2 - G1**2 / 4
+    assert np.imag(s1) == -M1 * G1
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,61 @@
 import numpy as np
 from dispersionrelations.utils import *
 
+
 def test_sqrt_along_rhc():
     sample_points = np.linspace(0, 1e4, 10)
     np.testing.assert_allclose(np.sqrt(sample_points), sqrtRHC(sample_points))
     np.testing.assert_allclose(np.sqrt(sample_points), np.imag(sqrtLHC(sample_points)))
+
+
+def assert_reflection(f, z, cut_angle=0.0, cut_in_imag=True):
+    z_refl = np.conj(z)
+    rotation = np.exp(1j * cut_angle)
+    z_rot = rotation * z
+    z_refl_rot = rotation * z_refl
+    f_val = f(z_rot)
+    f_val_refl = f(z_refl_rot)
+    if cut_in_imag:
+        np.testing.assert_allclose(f_val, np.conj(f_val_refl))
+    else:
+        np.testing.assert_allclose(f_val, -np.conj(f_val_refl))
+
+
+def test_sqrt_reflection():
+    sample_points = np.linspace(-1e4, 1e4, 3) + 1j * np.linspace(-1e2, 1e2, 3) ** 2
+    assert_reflection(sqrtRHC, sample_points, cut_in_imag=False)
+    assert_reflection(sqrtLHC, sample_points, cut_in_imag=False)
+    # np.random.seed(42)
+    test_angle = (np.random.rand() - 0.5) * 2 * np.pi
+    assert_reflection(
+        lambda z: sqrt_custom_branch_cut(z, test_angle),
+        sample_points,
+        test_angle,
+        cut_in_imag=False,
+    )
+
+
+def test_log_reflection():
+    sample_points = np.linspace(-1e4, 1e4, 4) + 1j * np.linspace(-1e2, 1e2, 4) ** 2
+    assert_reflection(lambda z: logRHC(z) - 1j * np.pi, sample_points, cut_in_imag=True)
+    assert_reflection(lambda z: logLHC(z) - 1j * np.pi, sample_points, cut_in_imag=True)
+    assert_reflection(lambda z: logC(z) + 1j * np.pi, sample_points, cut_in_imag=True)
+    # np.random.seed(42)
+    test_angle = (np.random.rand() - 0.5) * 2 * np.pi
+    assert_reflection(
+        lambda z: log_custom_branch_cut(z, test_angle) - 1j * np.pi,
+        sample_points,
+        test_angle,
+        cut_in_imag=True,
+    )
+
+
+def test_phase_extraction():
+    E_1 = np.linspace(0, 1.1, 1000)
+    s_1 = E_1**2
+    t_1 = 2 * np.pi * 3 * np.sin(2 * np.pi * s_1)
+    t_2 = 0.0 * s_1
+    t_3 = -t_1
+    np.testing.assert_allclose(extract_phase(np.exp(1j * t_1)), t_1)
+    np.testing.assert_allclose(extract_phase(np.exp(1j * t_2)), t_2)
+    np.testing.assert_allclose(extract_phase(np.exp(1j * t_3)), t_3)
